8cae1c502cbef7b13ecb6ef77cd9cc4f457cec66
Lucas Braga <lucas26raphael@hotmail.com>
2023-08-10 17:10:19 -0300

    att

--- Java-Core/src/application/program.java
+++ Java-Core/src/application/program.java
@@ -22,14 +22,32 @@ public class program {
 		X.b=in.nextDouble();
 		X.c=in.nextDouble();
 		
-		System.out.println("Informe as medidas do triangulo X: ");
+
+		System.out.println("Informe as medidas do triangulo Y: ");
 		Y.a=in.nextDouble();
 		Y.b=in.nextDouble();
 		Y.c=in.nextDouble();		
 		
+		if(X.a == X.b && X.b == X.c) {
+			System.out.println("EQUILATERO");
+		}else if(X.a == X.b || X.b == X.c || X.c == X.a) {
+			System.out.println("ESCALENO");
+		}else {
+			System.out.println("ISÓCELES");
+		}
+		
+		if(Y.a == Y.b && Y.b == Y.c) {
+			System.out.println("EQUILATERO");
+		}else if(Y.a == Y.b || Y.b == Y.c || Y.c == Y.a) {
+			System.out.println("ESCALENO");
+		}else {
+			System.out.println("ISÓCELES");
+		}
+		
 		
 		double areaDeX = X.area();
 		double areaDeY = Y.area();
+		
 		
 		System.out.printf("A area do triangulo X é: %.2f%n",areaDeX);
 		System.out.printf("A area do triangulo Y é: %.2f",areaDeY);		
