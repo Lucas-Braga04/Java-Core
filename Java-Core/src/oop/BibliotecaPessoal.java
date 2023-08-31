972734a4385d3026c7d44689d63f4b4f6a44cf0a
tci-dev <tci-dev@192.168.66.32>
2023-08-30 21:25:12 -0300

    att

---
+++ Java-Core/src/oop/BibliotecaPessoal.java
@@ -0,0 +1,25 @@
+package oop;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class BibliotecaPessoal {
+
+	public static void main(String[] args) {
+		
+		List<String> livros = new ArrayList<>();
+		
+		
+		livros.add("1");
+		livros.add("As Cronica de Narnia");
+		livros.add("C. S Lewis");
+		livros.add("1950");
+		livros.add("Fantasia, aventura");
+		livros.add("HarperCollins Publishers LLC");
+		livros.add("184");
+		
+		System.out.println(livros);
+		
+		
+	}
+}
--- Java-Core/src/oop/Listas.java
+++ Java-Core/src/oop/Listas.java
@@ -55,13 +55,30 @@ public class Listas {
 		
 		//Listar posicão de um elemento
 		System.out.println("Index em inglês: " + lista.indexOf("Inglês"));
-
+		System.out.println("---------------------------");
+		for (String materias : lista) {
+			System.out.println(materias);
+		}
+		System.out.println("--------------------------");
 		
 		//Trabalhar com predicado 
 		//Remover todos os elementos que iniciem com a letra G
 		//Para remover um elementos utilizando predicado, usamos o método removeif
 		
-		lista.removeIf(disciplina -> disciplina.charAt(0) == 'G');
+		lista.removeIf(disciplina -> disciplina.charAt(0) == 'G');// Remover toda String disciplina, tal que disciplina na posição 0 seja == 'G'
+		
+		for (String materias : lista) {
+			System.out.println(materias);
+			
+		}
+		System.out.println("------------------------");
+		lista.removeIf(disciplina -> disciplina.charAt(4) == 'm');
+		lista.removeIf(disciplina -> disciplina.charAt(2) == 'm');
+		
+		for (String materias : lista) {
+			System.out.println(materias);
+			
+		}
 		
 	}
 
